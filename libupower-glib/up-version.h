b273b0b88af17a7e4d2739fb63df7e307ebc89fa
Martin Pitt <martin.pitt@ubuntu.com>
2011-09-08 12:33:44 +0200

    Imported Upstream version 0.9.13

--- libupower-glib/up-version.h
+++ libupower-glib/up-version.h
@@ -55,7 +55,7 @@
  *
  * The compile-time micro version
  */
-#define UP_MICRO_VERSION				(12)
+#define UP_MICRO_VERSION				(13)
 
 /* check whether a UPower version equal to or greater than
  * major.minor.micro.
