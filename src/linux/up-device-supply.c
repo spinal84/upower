7c4e0edb11154e92e5991bc8da06ac41249dfc36
Arthur Demchenkov <spinal.by@gmail.com>
2018-12-12 16:43:25 +0300

    Coldplug property: energy-full-design
    No need to refresh it every time.
    Should go to upstream.

--- src/linux/up-device-supply.c
+++ src/linux/up-device-supply.c
@@ -666,6 +666,7 @@ up_device_supply_refresh_battery (UpDeviceSupply *supply,
 		}
 		g_object_set (device,
 			      "capacity", capacity,
+			      "energy-full-design", energy_full_design,
 			      "voltage-min-design", voltage_min_design,
 			      "voltage-max-design", voltage_max_design,
 			      NULL);
@@ -678,7 +679,6 @@ up_device_supply_refresh_battery (UpDeviceSupply *supply,
 			      "charge-full", &charge_full,
 			      "energy-full", &energy_full,
 			      "charge-full-design", &charge_full_design,
-			      "energy-full-design", &energy_full_design,
 			      NULL);
 	}
 
@@ -861,7 +861,6 @@ up_device_supply_refresh_battery (UpDeviceSupply *supply,
 		      "charge-full-design", charge_full_design,
 		      "energy", energy,
 		      "energy-full", energy_full,
-		      "energy-full-design", energy_full_design,
 		      "energy-rate", energy_rate,
 		      "percentage", percentage,
 		      "state", state,
